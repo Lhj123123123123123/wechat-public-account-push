568ed14029acbb7020101b039f520f5719004d56
Lhj123123123123123 <114323462+Lhj123123123123123@users.noreply.github.com>
2022-09-25 12:50:55 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -67,7 +67,7 @@ const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'XV0CGvvOBi5Z7dprC9hV4Q-ZRs-n61vVHZJLAUMbf9M',
       // 所在省份
       province: '山西',
       // 所在城市
