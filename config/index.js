e1325e19f2b6fd265a29afe30aba2064e903d8d5
Lhj123123123123123 <114323462+Lhj123123123123123@users.noreply.github.com>
2022-09-25 12:54:33 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -60,14 +60,14 @@ const USER_CONFIG = {
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
    */
-  USERS: [oGnUj597jU10S5VqnlpGwKP_VVbY',
+  USERS: [oGnUj597jU10S5VqnlpGwKP_VVbY
     {
       // 想要发送的人的名字
       name: '老婆0',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: 'XV0CGvvOBi5Z7dprC9hV4Q-ZRs-n61vVHZJLAUMbf9M',
+      useTemplateId: '	XV0CGvvOBi5Z7dprC9hV4Q-ZRs-n61vVHZJLAUMbf9M',
       // 所在省份
       province: '山西',
       // 所在城市
